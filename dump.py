5f6e11ada85c01db01d1aba4d886dc0a9dad5817
netos23 <nikmoroz2002@outlook.com>
2023-11-04 02:59:27 +0300

    refactor(dump) replase docker with docker compose

--- dump.py
+++ dump.py
@@ -2,7 +2,7 @@ import os
 from subprocess import Popen
 
 
-def dump_schema(path):
+def dump_schema(path="test.sql"):
     _dump_schema(
         os.environ['PGHOSTNAME'],
         os.environ['PGDATABASE'],
@@ -13,17 +13,22 @@ def dump_schema(path):
 
 
 def _dump_schema(host, dbname, user, password, path, **kwargs):
-    bd_docker_name = os.environ['DBDOCKERNAME']
+    bd_docker_name = os.environ['DBDOCKERDBNAME']
+    bd_docker_compose = os.environ['DBDOCKERLOCATION']
+
     docker_command_extension = ''
     if bd_docker_name is not None and bd_docker_name != "":
-        docker_command_extension = f'docker exec {bd_docker_name} /usr/bin/'
+        docker_command_extension = f'docker compose ' \
+                                   f'-f {bd_docker_compose}' \
+                                   f'exec {bd_docker_name}'
+
     command = f'{docker_command_extension}pg_dump --host={host} ' \
               f'--dbname={dbname} ' \
               f'--username={user} ' \
+              f'--inserts ' \
               f'> {path} '
     print(command)
     proc = Popen(command, shell=True, env={
         'PGPASSWORD': password
     })
     return proc.communicate(password)
-
