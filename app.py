891caf08eb0fc0783ae93d0e853777398bb01c89
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-11-03 23:05:11 +0300

    add endpoints

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ import get_info
 app = Flask(__name__)
 
 
+@app.route('/restart_db')
+def get_metrics():  # put application's code here
+    return 'hello'
+
+
+@app.route('/restore_db')
+def get_metrics():  # put application's code here
+    return 'hello'
+
+
+@app.route('/dump_db')
+def get_metrics():  # put application's code here
+    return 'hello'
+
+
 @app.route('/get_metrics')
 def get_metrics():  # put application's code here
     result = get_info.get_info()
