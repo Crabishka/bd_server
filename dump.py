91f78bbd385e9c3467fce28e3ecf5d393732b7a3
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-11-04 02:51:39 +0300

    create backup dir

--- dump.py
+++ dump.py
@@ -17,7 +17,7 @@ def _dump_schema(host, dbname, user, password, path, **kwargs):
     docker_command_extension = ''
     if bd_docker_name is not None and bd_docker_name != "":
         docker_command_extension = f'docker exec -i {bd_docker_name} /usr/bin/'
-    command = f'touch path'
+    command = f'touch {path}'
     proc = Popen(command, shell=True)
     command = f'{docker_command_extension}pg_dump --host={host} ' \
               f'--dbname={dbname} ' \
