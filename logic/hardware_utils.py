49866e78191db51af876ad7d2d35618afd74417f
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-11-04 23:08:56 +0300

    fix hardware_data

--- logic/hardware_utils.py
+++ logic/hardware_utils.py
@@ -1,13 +1,17 @@
 import json
 import os
+from subprocess import Popen
 
 
 def get_hardware_utils():
     db_name = os.environ['PGDATABASE']
     command = f'docker stats --no-stream --format   "{{ json . }}"  {db_name}'
-    os.system(f'{command} > tmp')
+    proc = Popen(f'{command} > tmp')
+    proc.wait()
     data = open('tmp', 'r').read()
     print(data)
-    hardware_data = json.loads(data)
-    return hardware_data
+    if data != "":
+        hardware_data = json.loads(data)
+        return hardware_data
+    return {}
 
