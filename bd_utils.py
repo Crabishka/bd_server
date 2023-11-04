5378143c9c1b7626449fdfedaba9e8402a8c32ce
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-11-04 18:31:52 +0300

    fix 500

--- bd_utils.py
+++ bd_utils.py
@@ -4,7 +4,7 @@ import psycopg2
 
 
 def get_connection():
-    connection = psycopg2.connect(
+    return psycopg2.connect(
         user=os.environ['PGUSERNAME'],
         password=os.environ['PGPASSWORD'],
         host=os.environ['PGHOSTNAME'],
