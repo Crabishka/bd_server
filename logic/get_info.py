03459fefa80c84d39b6b658c4791f875e8d5a8c3
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-11-04 23:52:52 +0300

    add size metric

--- logic/get_info.py
+++ logic/get_info.py
@@ -6,6 +6,11 @@ from logic.hardware_utils import get_hardware_utils
 sql_item = {
     'Alive': "select 1;",  # monitor survival
     'Cache hit ratio': "SELECT sum(heap_blks_hit) / (sum(heap_blks_hit) + sum(heap_blks_read)) as ratio FROM pg_statio_user_tables;",
+    'Size': """
+    SELECT  pg_size_pretty(pg_tablespace_size(spcname)) as size
+    FROM pg_tablespace
+    WHERE spcname<>'pg_global';
+    """,
     'Active_connections': "select count (*) from pg_stat_activity where state = 'active';",
     'Server_connections': "select count (*) from pg_stat_activity where backend_type = 'client backend'",
     'Idle_connections': "select count (*) from pg_stat_activity where state = 'idle'",
@@ -62,7 +67,6 @@ def get_info():
         max_value = None
         if '/' in value:
             splitted = value.split('/')
-            print(splitted)
             value = splitted[0].strip()
             max_value = splitted[1].strip()
         data = {'timestamp': now, 'name': key, 'value': value, 'max_value': max_value}
